3e2a8c92a27127f7023b539f13db765fe5f9a35d
julianamonr03 <julianamonroy03@hotmail.com>
2020-08-11 21:00:52 +0000

    headers

---
+++ holberton.h
@@ -0,0 +1,8 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdlib.h>	/* malloc, exit */
+#include <stdio.h>	/* printf, getline, stdin */
+
+int _getline(char **lineptr, size_t *n, FILE *stream);
+
+#endif /* _HOLBERTON_H_ */
