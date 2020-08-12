7368cba6764489f5723219fa1e4286a3aea7202a
julianamonr03 <julianamonroy03@hotmail.com>
2020-08-12 19:19:52 +0000

    Update header

--- holberton.h
+++ holberton.h
@@ -2,7 +2,10 @@
 #define _HOLBERTON_H_
 #include <stdlib.h>	/* malloc, exit */
 #include <stdio.h>	/* printf, getline, stdin */
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int _getline(char **lineptr, size_t *n, FILE *stream);
-
+void _puts(char *str);
+int _putchar(char c);
 #endif /* _HOLBERTON_H_ */
