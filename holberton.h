7ba4cf48694fecba1472c5e8f17739dc58f8c82e
Athesto <gamez.live@gmail.com>
2020-08-12 20:37:05 -0500

    unitstd, string, signal

--- holberton.h
+++ holberton.h
@@ -1,9 +1,12 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-#include <stdlib.h>	/* malloc, exit */
+#include <stdlib.h>	/* malloc, exit, free */
 #include <stdio.h>	/* printf, getline, stdin */
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <sys/types.h> /* ssize_t */
+#include <sys/wait.h> /* wait */
+#include <unistd.h>  /* size_t, write */
+#include <string.h> /* strtok */
+#include <signal.h> /* signal */
 
 int _getline(char **lineptr, size_t *n, FILE *stream);
 void _puts(char *str);
