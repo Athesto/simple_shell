e2ca0ad8bc13b83d5d4236c79d60be789c95810d
Athesto <gamez.live@gmail.com>
2020-08-12 20:43:35 -0500

    loop function

--- simple_shell.c
+++ simple_shell.c
@@ -16,27 +16,58 @@ int main(void)
 
 	return (EXIT_SUCCESS);
 }
+
+#define BUFSIZE 1
+#define nargs 16
+#define splitchars " \t"
 /**
  * loop - shell loop
  */
 void loop(void)
 {
-	char *PS1 = "#jgsh$ "; /* prompt */
 
 	char *line;
-	size_t n = 1024;
+	char **args;
+	size_t n = BUFSIZE;
+	int i;
 	ssize_t nbytes;
+	char *PS1 = PROMPT; /* prompt */
 
-	line = malloc(32);
 
+	args = malloc(nargs * sizeof(*args));
+
+	line = malloc(n * sizeof(*line));
 	while (TRUE)
 	{
 		_puts(PS1);
 		nbytes = getline(&line, &n, stdin);
 		if (nbytes == EOF)
+		{
+			_putchar('\n');
 			break;
+		}
+		line[nbytes - 1] = 0;
 		printf("(%d) %s\n", (int)nbytes, line);
+
+		/* cmd */
+		args[0] = strtok(line, splitchars);
+
+		i = 1;
+		while ((args[i] = strtok(NULL, splitchars)) != NULL)
+		{
+			i++;
+		}
+		i = 0;
+		while (args[i])
+		{
+			printf("%s\n", args[i]);
+			i++;
+		}
 	}
+	free(line);
+	free(args);
+	(void)args;
+}
 /**
  * handler - interruption routine
  * @num: ?
