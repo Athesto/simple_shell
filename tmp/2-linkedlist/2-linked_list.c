d2ffb0862d39a519bab37ad949fce23f6e4da9e2
Athesto <gamez.live@gmail.com>
2020-08-16 20:16:25 -0500

    linked list example

--- tmp/2-linkedlist/2-linked_list.c
+++ tmp/2-linkedlist/2-linked_list.c
@@ -3,8 +3,8 @@
 #include "../../holberton.h"
 #include <string.h>
 /**
- * main - ?
- * Return: ?
+ * main - get PATH as Linked list
+ * Return: 0 Always
  */ 
 int main(void)
 {
@@ -15,33 +15,36 @@ int main(void)
 
 	cp_PATH = _getpath();	/* cp_PATH pointing to PATH in ENV */
 	PATH_backup = cp_PATH;	/* backup of pointer */
-	/* move cp_PATH to the next position after "PATH=" in the PATH */
-	while ((*cp_PATH++) != '=') 
+	/* move cp_PATH after "PATH=" in the PATH variable */
+	/*
+	 * 1. eval *cp_PATH  != '='
+	 * 2. increase cp_PATH
+	 * 3. repeat or skip
+	 */
+	while ((*cp_PATH++) != '=')
 		;
-
-	/* duplicate string at cp_PATH and cp_PATH points to new location */
-	cp_PATH = strdup(cp_PATH); 
- 	/* changed 1rst ':' to '\0' and tmp = cp_PATH */
-	tmp = strtok(cp_PATH, ":"); 	
-	/* duplicate the string pointed by tmp into a node at end of list */
-	add_node_end(&head, tmp); 
-
+	/* duplicate the string pointd by cp_PATH and cp_PATH points to new location */
+	cp_PATH = strdup(cp_PATH);
+ 	/* 1: changed 1rst ':' to '\0' (this operation split cp_PATH at ':')
+	 * 2: tmp points to left side of splited cp_PATH */ 
+	tmp = strtok(cp_PATH, ":");
+	/* duplicate the string pointed by tmp and add it to node at end of list */
+	add_node_end(&head, tmp);
 	/* 
-	 * 1: set the frist ':' into '\0' of cp_PATH's leaftover
-	 * 2: set tmp pointing to the new position
+	 * 1: set the next ':' into '\0' of right side of cp_PATH 
+	 * (this operation split the right side of cp_PATH)
+	 * 2: tmp points to the left side of the slipted string 
 	 * 3: check if tmp is NULL
+	 * 4: repeat until convert all ':' into '\0'
 	*/	
 	while ((tmp = strtok(NULL, ":")))
 	{
 		/* duplicate the string pointed by tmp into a node at end of list */
 		add_node_end(&head, tmp);
 	}
-	
 	print_list(head);	/* print list */
 	free_list(head);	/* free list */
 	
-	puts(PATH_backup);	/* Debug: PATH */
-	puts("=========");
 	free(PATH_backup); 	/* free PATH */
 	return (0);
 }
