189129f60f991e2e32db00dc4d5ec05a4ce1f513
Athesto <gamez.live@gmail.com>
2020-08-19 15:23:28 -0500

    6-tst change output format

--- tmp/6-penv.c
+++ tmp/6-penv.c
@@ -4,19 +4,21 @@ int main(int argc, char *argv[], char *env[])
 	extern char **environ;
 	int i;
 
+	puts("\n\n\n\n\n==============");
 	i = 0;
 	while(env[i])
 	{
 		printf("%s\n", env[i]);
 		i++;
 	}
-	puts("\n\n\n\n\n==============\n\n\n\n");
+	puts("\n\n==============\n\n");
 	i = 0;
 	while(environ[i])
 	{
 		printf("%s\n", environ[i]);
 		i++;
 	}
+	puts("==============\n\n\n\n");
 	(void)argc;
 	(void)argv;
 	return (0);
