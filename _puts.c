6669186754e08240e7fdf7eb1d86e6f51b2324af
julianamonr03 <julianamonroy03@hotmail.com>
2020-08-19 02:31:58 +0000

    Adding fputs function

--- _puts.c
+++ _puts.c
@@ -8,3 +8,20 @@ void _puts(char *str)
 	while (*str)
 		_putchar(*str++);
 }
+
+/**
+ * _fputs - put string
+ * @fd: file descriptor
+ * @str: input string
+ */
+void _fputs(int fd, char *str)
+{
+	int s_idx;
+
+	s_idx = 0;
+	while (str[s_idx])
+	{
+		s_idx++;
+	}
+	write(fd, str, s_idx);
+}
