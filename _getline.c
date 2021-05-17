ac4e3a2f385f827be248236ef10efa2e17ffe3f2
Athesto <gamez.live@gmail.com>
2021-05-17 18:35:09 -0500

    _getline as getline

--- _getline.c
+++ _getline.c
@@ -8,19 +8,6 @@
  */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
-	char *local_line;
-	ssize_t bytes;
-	int i;
 
-	bytes = getline(lineptr, n, stream);
-	if (bytes == EOF)
-		return (bytes);
-
-	local_line = *lineptr;
-	for (i = 0; local_line[i]; i++)
-	{
-		if (local_line[i] == '\n')
-			local_line[i] = '\0';
-	}
-	return (bytes);
+	return (getline(lineptr, n, stream));
 }
