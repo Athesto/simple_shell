c0b39411dfb2943643c84d6c68a2e31b79d95fa8
Athesto <gamez.live@gmail.com>
2021-05-26 20:00:33 -0500

    resolve output order in /bin/ls

--- _runcmd.c
+++ _runcmd.c
@@ -10,11 +10,17 @@ int _runcmd(char const *cmd, char *const *args)
 {
 	int status = 0;
 	pid_t my_pid;
+	char *myenv[2] = {
+		"LC_ALL=",
+		NULL
+	};
+
+	myenv[0] = _getenv("LC_ALL") - _strlen("LC_ALL=");
 
 	my_pid = fork(); /* my_pid@child = 0; my_pid@parent = my_pid@child*/
 	if (my_pid == 0)
 	{
-		status = execve(cmd, args, environ);
+		status = execve(cmd, args, myenv);
 		exit(status);
 	}
 	else
